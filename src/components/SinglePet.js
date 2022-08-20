12af3e146059b5a6b1b4ea3f04ecddc7458d257a
bbrozen <bbrozen@fordham.edu>
2022-08-20 00:44:00 -0400

    please work

--- src/components/SinglePet.js
+++ src/components/SinglePet.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 function SinglePet() {
-
+  //const [list, setList] =React.useState([])
   return (
     <div className={`single-pet`}>
-
+        {/* <h1>{props.pet.name}</h1>
+        <h2>{props.pet.species}</h2>
+        <p>{props.pet.description}</p>   */}
     </div>
   );
 }
