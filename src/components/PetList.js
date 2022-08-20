546dc3e44fb78158b456ab323b03bade9766961b
bbrozen <105470844+bbrozen@users.noreply.github.com>
2022-08-20 19:17:34 -0400

    Update PetList.js

--- src/components/PetList.js
+++ src/components/PetList.js
@@ -8,14 +8,35 @@ const cody = {
   species: 'dog',
 };
 
+
+
 // PetList only renders one SinglePet. We'd like it to render a list of pets,
 // passed in as props.pets. Don't forget to add a unique key to each one!
-function PetList() {
+function PetList(props) {
+
+  const [list, setList] = React.useState(props.pets);
+  const [species, setSpecies] = React.useState("all");
 
   return (
     <>
+     <label for="pets">Choose a type of pet: </label>
+      <select name="pets" id="pets" onChange={(event)=> 
+        setSpecies(event.target.value)
+      }>
+        <option value="cat">Cats</option>
+        <option value="dog">Dogs</option>
+        <option value="all">All</option>
+      </select>
+
       <div className="pet-list">
-            <SinglePet  pet={cody}  />
+      { list.filter(itm =>{
+        if(species === "all"){
+            return itm;
+        }
+        return itm.species === species})
+            .map(itm =>  
+            <SinglePet key={itm.id} pet={itm}  />
+        )}
       </div>
     </>
   )
